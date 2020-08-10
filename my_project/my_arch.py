bbcd01b259e57c40de4459853a3179a90eb06fcd
vsong <18221188857@163.com>
2020-08-10 21:45:27 +0800

    0.5

--- my_project/my_arch.py
+++ my_project/my_arch.py
@@ -19,7 +19,7 @@ from pycore.blocks import *
 #     to_connection("pool2", "soft1"),
 #     to_end()
 #     ]
-
+opacity=0.6
 arch = [
     to_head('..'),
     to_cor(),
@@ -29,41 +29,43 @@ arch = [
     to_input('../examples/fcn8s/cats.jpg'),
 
     # block-001
-    to_ConvConvRelu(name='ccr_b1', s_filer=500, n_filer=(64, 64), offset="(0,0,0)", to="(0,0,0)", width=(2, 2),
+    to_ConvRelu(name='ccr_b1', s_filer=500, n_filer=64, offset="(0,0,0)", to="(0,0,0)", width=2,
                     height=40, depth=40),
-    to_Pool(name="pool_b1", offset="(0,0,0)", to="(ccr_b1-east)", width=1, height=32, depth=32, opacity=0.5),
+    to_Pool(name="pool_b1", offset="(0,0,0)", to="(ccr_b1-east)", width=1, height=32, depth=32, opacity=opacity),
 
-    *block_2ConvPool(name='b2', botton='pool_b1', top='pool_b2', s_filer=256, n_filer=128, offset="(1,0,0)",
+    *block_1ConvPool(name='b2', botton='pool_b1', top='pool_b2', s_filer=256, n_filer=128, offset="(2,0,0)",
                      size=(32, 32, 3.5), opacity=0.5),
-    *block_2ConvPool(name='b3', botton='pool_b2', top='pool_b3', s_filer=128, n_filer=256, offset="(1,0,0)",
+    *block_1ConvPool(name='b3', botton='pool_b2', top='pool_b3', s_filer=128, n_filer=256, offset="(2,0,0)",
                      size=(25, 25, 4.5), opacity=0.5),
-    *block_2ConvPool(name='b4', botton='pool_b3', top='pool_b4', s_filer=64, n_filer=512, offset="(1,0,0)", size=(16, 16, 5.5), opacity=0.5),
+    *block_1ConvPool(name='b4', botton='pool_b3', top='pool_b4', s_filer=64, n_filer=512, offset="(1.5,0,0)", size=(16, 16, 5.5), opacity=0.5),
 
     # Bottleneck
-    # block-005
-    # to_ConvConvRelu(name='ccr_b5', s_filer=32, n_filer=(1024, 1024), offset="(2,0,0)", to="(pool_b4-east)",
-    #                 width=(8, 8), height=8, depth=8, caption="Bottleneck"),
     # to_SoftMax("soft1", 10, "(3,0,0)", "(pool1-east)", caption="SOFT"),
-    to_FullyConnected(name="soft1", s_filer=4096, offset="(1.25,0,0)", to="(pool_b4-east)", width=1, height=1, depth=20,
+    to_FullyConnected(name="fc1", s_filer=4096, offset="(2,0,0)", to="(pool_b4-east)", width=1.6, height=1.6, depth=50,
                       caption="fl"),
-    to_connection("pool_b4", "soft1"),
+    to_connection("pool_b4", "fc1"),
+    to_FullyConnected(name="fc3", s_filer=4096, offset="(2,0,4)", to="(fc1-east)", width=1.6, height=1.6, depth=50,
+                      caption="f3"),
+    to_FullyConnected(name="fc2", s_filer=4096, offset="(2,0,-4)", to="(fc3-east)", width=1.6, height=1.6, depth=50,
+                      caption="f2"),
+    # to_connection("fc1", "fc2"),
 
     # Decoder
-    *block_Unconv(name="b6", botton="soft1", top='end_b6', s_filer=64, n_filer=512, offset="(2.1,0,0)",
-                  size=(16, 16, 5.0), opacity=0.5),
-    to_skip(of='ccr_b4', to='ccr_res_b6', pos=1.25),
-    *block_Unconv(name="b7", botton="end_b6", top='end_b7', s_filer=128, n_filer=256, offset="(2.1,0,0)",
-                  size=(25, 25, 4.5), opacity=0.5),
-    to_skip(of='ccr_b3', to='ccr_res_b7', pos=1.25),
-    *block_Unconv(name="b8", botton="end_b7", top='end_b8', s_filer=256, n_filer=128, offset="(2.1,0,0)",
-                  size=(32, 32, 3.5), opacity=0.5),
-    to_skip(of='ccr_b2', to='ccr_res_b8', pos=1.25),
+    *block_1Unconv(name="b6", botton="fc2", top='end_b6', s_filer=64, n_filer=512, offset="(2,0,0)",
+                  size=(16, 16, 5.0), opacity=opacity),
+    to_skip(of='ccr_b4', to='end_b6', pos=1.25),
+    *block_1Unconv(name="b7", botton="end_b6", top='end_b7', s_filer=128, n_filer=256, offset="(2,0,0)",
+                  size=(25, 25, 4.5), opacity=opacity),
+    to_skip(of='ccr_b3', to='end_b7', pos=1.25),
+    *block_1Unconv(name="b8", botton="end_b7", top='end_b8', s_filer=256, n_filer=128, offset="(2,0,0)",
+                  size=(32, 32, 3.5), opacity=opacity),
+    to_skip(of='ccr_b2', to='end_b8', pos=1.25),
 
-    *block_Unconv(name="b9", botton="end_b8", top='end_b9', s_filer=512, n_filer=64, offset="(2.1,0,0)",
-                  size=(40, 40, 2.5), opacity=0.5),
-    to_skip(of='ccr_b1', to='ccr_res_b9', pos=1.25),
+    *block_1Unconv(name="b9", botton="end_b8", top='end_b9', s_filer=512, n_filer=64, offset="(2.5,0,0)",
+                  size=(40, 40, 2.5), opacity=opacity),
+    # to_skip(of='ccr_b1', to='ccr_res_b9', pos=1.25),
 
-    to_ConvSoftMax(name="soft1", s_filer=512, offset="(0.75,0,0)", to="(end_b9-east)", width=1, height=40, depth=40,
+    to_ConvSoftMax(name="soft1", s_filer=512, offset="(2,0,0)", to="(end_b9-east)", width=1, height=40, depth=40,
                    caption="SOFT"),
     to_connection("end_b9", "soft1"),
 
--- pycore/blocks.py
+++ pycore/blocks.py
@@ -33,6 +33,31 @@ def block_2ConvPool( name, botton, top, s_filer=256, n_filer=64, offset="(1,0,0)
         )
     ]
 
+def block_1ConvPool( name, botton, top, s_filer=256, n_filer=64, offset="(1,0,0)", size=(32,32,3.5), opacity=0.5 ):
+    return [
+    to_ConvRelu(
+        name="ccr_{}".format( name ),
+        s_filer=str(s_filer),
+        n_filer=n_filer,
+        offset=offset,
+        to="({}-east)".format( botton ),
+        width=size[2],
+        height=size[0],
+        depth=size[1],
+        ),
+    to_Pool(
+        name="{}".format( top ),
+        offset="(0,0,0)",
+        to="(ccr_{}-east)".format( name ),
+        width=1,
+        height=size[0] - int(size[0]/4),
+        depth=size[1] - int(size[0]/4),
+        opacity=opacity, ),
+    to_connection(
+        "{}".format( botton ),
+        "ccr_{}".format( name )
+        )
+    ]
 
 def block_Unconv( name, botton, top, s_filer=256, n_filer=64, offset="(1,0,0)", size=(32,32,3.5), opacity=0.5 ):
     return [
@@ -47,7 +72,18 @@ def block_Unconv( name, botton, top, s_filer=256, n_filer=64, offset="(1,0,0)", 
             )
     ]
 
-
+def block_1Unconv( name, botton, top, s_filer=256, n_filer=64, offset="(1,0,0)", size=(32,32,3.5), opacity=0.5 ):
+    return [
+        to_UnPool(  name='uppool_{}'.format(name),    offset=offset,    to="({}-east)".format(botton),         width=1,              height=size[0],       depth=size[1], opacity=opacity ),
+        to_ConvRes( name='{}'.format(top),   offset="(0,0,0)", to="(uppool_{}-east)".format(name),    s_filer=str(s_filer), n_filer=str(n_filer), width=size[2], height=size[0], depth=size[1], opacity=opacity ),
+        # to_Conv(    name='ccr_{}'.format(top),       offset="(0,0,0)", to="(ccr_res_{}-east)".format(name),   s_filer=str(s_filer), n_filer=str(n_filer), width=size[2], height=size[0], depth=size[1] ),
+        # to_ConvRes( name='ccr_res_c_{}'.format(name), offset="(0,0,0)", to="(ccr_{}-east)".format(name),       s_filer=str(s_filer), n_filer=str(n_filer), width=size[2], height=size[0], depth=size[1], opacity=opacity ),
+        # to_Conv(    name='{}'.format(top),            offset="(0,0,0)", to="(ccr_res_c_{}-east)".format(name), s_filer=str(s_filer), n_filer=str(n_filer), width=size[2], height=size[0], depth=size[1] ),
+        to_connection(
+            "{}".format( botton ),
+            "uppool_{}".format( name )
+            )
+    ]
 
 
 def block_Res( num, name, botton, top, s_filer=256, n_filer=64, offset="(0,0,0)", size=(32,32,3.5), opacity=0.5 ):
